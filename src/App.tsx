734d1de4bef6fec337cd7fad039db9c258b1fa85
dat.ngotri.ncc <dat.ngotri.ncc@gmail.com>
2021-04-16 23:07:44 +0700

    feat: add component item

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,24 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/header/Header";
+import { Col, Container, Row } from "react-bootstrap";
+import Item from "./components/item/Item";
 
 function App() {
   return (
     <div>
       <div className="header">
         <Header />
+      </div>
+      <div className="wrapper">
+        <div className="container-fluid">DashBoard</div>
+        <Container>
+          <Row>
+            <Col xs={12} md={4}>
+              <Item />
+            </Col>
+          </Row>
+        </Container>
       </div>
     </div>
   );
